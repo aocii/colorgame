1e3279e0af2112b3e36127fe70b390d2909254eb
aocirey <aocirey@qualist.com>
2020-01-17 16:19:56 +0300

    second

--- jexercises.js
+++ jexercises.js
@@ -1,33 +1,75 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    var generateAll = function () {
+    var generateAll = function (type) {
         window.answers = document.querySelectorAll(".a");
 
         clean();
 
-        function generateColor() {
-            window.a1 = Math.round(Math.random() * 255);
-            window.a2 = Math.round(Math.random() * 255);
-            window.a3 = Math.round(Math.random() * 255);
+
+        function randomInt(min, max) {
+            return min + Math.floor((max - min) * Math.random());
+        }
+
+        function generateColor(type) {
+
+            if (type === "hard") {
+                if (Math.round(Math.random() * 2) == 0) {
+                    generateRGB(0, 85);
+                    console.log(11)
+                } else if (Math.round(Math.random() * 2) == 1) {
+                    console.log(22)
+                    generateRGB(85, 170);
+
+                } else {
+                    console.log(33)
+                    generateRGB(170, 255);
+                }
+            } else {
+                generateRGB(0, 255);
+            }
+
+        }
+
+        function generateRGB(x, y) {
+            window.a1 = randomInt(x, y);
+            window.a2 = randomInt(x, y);
+            window.a3 = randomInt(x, y);
             var generatedColor = "RGB(" + a1 + "," + a2 + "," + a3 + ")";
+
             return generatedColor;
         }
 
-        var redPercentage = 100-((255-window.a1)/255);
-        var greenPercentage = 100-((255-window.a2)/255);
-        var bluePercentage = 100-((255-window.a3)/255);
-
-
-
+        var singleColors = [];
         for (i = 0; i <= answers.length - 1; i++) {
-            answers[i].style.background = generateColor();
+            generateColor(type);
+            answers[i].style.background = "RGB(" + window.a1 + "," + window.a2 + "," + window.a3 + ")";
+            console.log(answers[i].style.background)
+            var singleColor = [];
+            singleColor.push(window.a1, window.a2, window.a3)
+            singleColors.push(singleColor);
 
         }
 
-        var answerOrder = Math.round(Math.random() * 9)-1;
+        var answerOrder = Math.round(Math.random() * 8);
+
         console.log(answerOrder);
         var question = document.querySelector("h1");
 
         question.textContent = answers[answerOrder].style.background;
+
+        var red = "RGB(" + singleColors[answerOrder][0] + ",0,0)";
+        var green = "RGB(0," + singleColors[answerOrder][1] + ",0)";
+        var blue = "RGB(0,0," + singleColors[answerOrder][2] + ")";
+
+        var redHtml = document.querySelector("#red");
+        var greenHtml = document.querySelector("#green");
+        var blueHtml = document.querySelector("#blue");
+        redHtml.style.background = red;
+        greenHtml.style.background = green;
+        blueHtml.style.background = blue;
+        redHtml.textContent = red;
+        greenHtml.textContent = green;
+        blueHtml.textContent = blue;
+
 
 
         for (i = 0; i <= answers.length - 1; i++) {
@@ -37,11 +79,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 clean();
                 window.self = this;
 
-                window.control = 0;
+                window.control = false;
                 if (self.style.background == question.textContent) {
 
                     window.foundAnswer = self.classList.add("right");
-                    window.control = 1;
+
+                    setTimeout(clean, 925);
+
+                    return window.contorl = true;
 
                 } else if (self.style.background != question.textContent) {
 
@@ -54,6 +99,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }
     }
     generateAll();
+
+
 
     function clean() {
 
@@ -69,8 +116,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     var generateButton = document.querySelector("#generateButton");
+    var hardButton = document.querySelector("#hard");
+    var easyButton = document.querySelector("#easy");
 
-    generateButton.addEventListener("click", generateAll, true);
+
+    var type = "";
+
+    hardButton.addEventListener("click", function () {
+        generateButton.textContent = "Generate Hard"
+        type = "hard";
+    });
+    easyButton.addEventListener("click", function () {
+        generateButton.textContent = "Generate Easy"
+        type = "easy"
+    });
+
+    generateButton.addEventListener("click", function () { generateAll(type) }, true);
 
 });
 
